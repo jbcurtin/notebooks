af6a62609064b50f173282483a98b2a0b4b8f919
Joseph Curtin <jcurtin@stsci.edu>
2019-09-20 14:49:11 -0400

    Updated CircleCI configuration to build each notebook+requirements for each notebook. Also added TestCase handling that'll report when errors are found with the pip install -r requirements.txt.

--- .circleci/setup_env.sh
+++ .circleci/setup_env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if [[ ! -d /opt/conda/envs/notebooks_env ]]; then
+    apt-get update
+    apt-get install build-essential gcc-4.8 -y
     conda info --envs
     conda env update --file=environment.yml
     source activate notebooks_env
