0879a8df2851162a79de69ad03be38fe7f9d4c0e
Joseph Curtin <jcurtin@stsci.edu>
2019-09-24 13:55:22 -0400

    experiment with pip install

--- .circleci/setup_env.sh
+++ .circleci/setup_env.sh
@@ -3,6 +3,7 @@
 if [[ ! -d /opt/conda/envs/notebooks_env ]]; then
     conda info --envs
     conda env update --file=environment.yml
+    apt-get install build-essential gcc -y
     source activate notebooks_env
     conda info --envs
 else
