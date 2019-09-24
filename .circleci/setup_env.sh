5e05728e32c7a8d14b186fb42c17a6107ab6b60c
Joseph Curtin <jcurtin@stsci.edu>
2019-09-24 13:51:56 -0400

    experiment with pip install

--- .circleci/setup_env.sh
+++ .circleci/setup_env.sh
@@ -4,8 +4,6 @@ if [[ ! -d /opt/conda/envs/notebooks_env ]]; then
     conda info --envs
     conda env update --file=environment.yml
     source activate notebooks_env
-    apt-get install build-essential gcc -y
-    pip install -e https://github.com/eteq/nbpages.git@b9ec8410803357939210e068af7e14a6f0625fab#egg=nbpages
     conda info --envs
 else
     echo "Using cached miniconda environment";
