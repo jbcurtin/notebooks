edf4ca7b9b7e922dd2361aa35b24866ce0fb7cb2
Joseph Curtin <jcurtin@stsci.edu>
2019-09-26 10:29:29 -0400

    updated gcc to gcc-4.8

--- .circleci/setup_env.sh
+++ .circleci/setup_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ ! -d /opt/conda/envs/notebooks_env ]]; then
-    apt-get install build-essential gcc -y
+    apt-get install build-essential gcc-4.8 -y
     conda info --envs
     conda env update --file=environment.yml
     source activate notebooks_env
