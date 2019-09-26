ecce000194fcd0b9c3bb93d0509b7c75d9cde11c
Joseph Curtin <jcurtin@stsci.edu>
2019-09-26 10:11:45 -0400

    removed filter

--- make_pages.py
+++ make_pages.py
@@ -17,7 +17,7 @@ logger = logging.getLogger(__file__)
 args = make_parser().parse_args()
 logger.info('Converting notebooks into HTML')
 converted = run_parsed('.', output_type='HTML', args=args)
-converted = [item for item in converted if not os.path.basename(item) in ['test-fail.html', 'test-succeed.html']]
+# converted = [item for item in converted if not os.path.basename(item) in ['test-fail.html', 'test-succeed.html']]
 
 logger.info('Creating HTML Index')
 make_html_index(converted, './index.tpl')
