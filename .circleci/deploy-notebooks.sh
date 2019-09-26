c64f9652749e449a20c400e4d2a117c586a002b0
Joseph Curtin <jcurtin@stsci.edu>
2019-09-26 13:39:50 -0400

    Added Deploy Pages routine

---
+++ .circleci/deploy-notebooks.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+if [ -z "${CIRCLE_PULL_REQUEST}" ]; then
+    git config --global user.email devnull@circleci.com
+    git config --global user.name CircleCI
+    mkdir -p ~/.ssh
+    echo 'Host * ' >> ~/.ssh/config
+    echo '  StrictHostKeyChecking no' >> ~/.ssh/config
+    # Deploy gh-pages
+    git clone -b gh-pages --single-branch ${CIRCLE_REPOSITORY_URL} /tmp/out
+    cd /tmp/out
+    git add .
+    git commit -m 'Automated deployment to Github Pages: ${BUILD_TAG}' -a || true
+    git push origin gh-pages || true
+    git clean -dfx
+fi
+exit 0
